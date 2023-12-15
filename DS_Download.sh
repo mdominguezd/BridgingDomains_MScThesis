ffcf66e756bea7bc26c8eec249caf876b05092f9
mdominguezd <martin.dominguez.duran@gmail.com>
2023-12-15 15:08:48 +0100

    Added ivory coast zip files

--- DS_Download.sh
+++ DS_Download.sh
@@ -5,8 +5,8 @@ wget -O Tanzania1.zip -q --show-progress https://www.dropbox.com/scl/fi/ueafajsk
 wget -O Tanzania2.zip -q --show-progress https://www.dropbox.com/scl/fi/pj7su9q8f422k06zy6oyf/PlanetTanzania_18_patch256_split2.zip?rlkey=8xw9al4h4i46h9c9b3azqa8e4&dl=0 
 wget -O Tanzania3.zip -q --show-progress https://www.dropbox.com/scl/fi/mhipwkf42vl8dluo4d6uu/PlanetTanzania_18_patch256_split3.zip?rlkey=2jtyw1n4u9wjibn3jnvduo717&dl=0 
 
-wget -O IvoryCoast1.zip -q --show-progress 
-wget -O IvoryCoast2.zip -q --show-progress 
-wget -O IvoryCoast3.zip -q --show-progress 
+wget -O IvoryCoast1.zip -q --show-progress https://www.dropbox.com/s/mn53y84ahj4y0vj/PlanetIvoryCoast_18_patch256_split1.zip?dl=0
+wget -O IvoryCoast2.zip -q --show-progress https://www.dropbox.com/scl/fi/6pxnc69et16gncsdd11ys/PlanetIvoryCoast_18_patch256_split2.zip?rlkey=xs0ae6o6nnpelkdi5gthq0x8v&dl=0
+wget -O IvoryCoast3.zip -q --show-progress https://www.dropbox.com/scl/fi/37ool0ctxlc43quovnvs7/PlanetIvoryCoast_18_patch256_split3.zip?rlkey=i8fs18b99vh0wmdvtj25gyhg5&dl=0
 
 
--- Dataset/Unzip_DS.py
+++ Dataset/Unzip_DS.py
@@ -1,5 +1,6 @@
 from zipfile import ZipFile
 import os 
+import warnings
 
 def UnzipFolders(domain):
     """
@@ -9,10 +10,13 @@ def UnzipFolders(domain):
             - domain: string with the name of the domain. Options: "Tanzania" or "IvoryCoast"
     """
     if (domain != "Tanzania") & (domain != "IvoryCoast"):
-        raise Exception("domain needs to be Tanzania or IvoryCoast")
+        raise Exception("Domain needs to be Tanzania or IvoryCoast")
 
     if len([i for i in os.listdir('.') if '.zip' in i]) != 0: 
-        for i in range(1): # NEEEDS TO BE CHANGED
-            with ZipFile(domain + str(i+1) + ".zip", 'r') as zipped:
-                zipped.extractall(path="./")
-            os.remove(domain + str(i+1) + ".zip")
+        for i in range(3): 
+            if (len([f for f in os.listdir('.') if domain + str(i+1) + ".zip" in f]) != 0):
+                with ZipFile(domain + str(i+1) + ".zip", 'r') as zipped:
+                    zipped.extractall(path="./")
+                os.remove(domain + str(i+1) + ".zip")
+            else:
+                raise warnings.warn("No zipped file found (" + domain + str(i+1) + ".zip" + ")")
